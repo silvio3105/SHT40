8e91a0a4f76b9223c14ce397dde72c92172248c6
silvio3105 <104866334+silvio3105@users.noreply.github.com>
2022-10-22 22:47:15 +0200

    Code for first release. Not tested!

---
+++ SHT40.cpp
@@ -0,0 +1,113 @@
+/**
+ * @file SHT40.cpp
+ * @author silvio3105
+ * @brief SHT40 C++ file
+ * @version v0.1r1
+ * @date 2022-10-22
+ * 
+ * @copyright silvio3105 (c) 2022
+ * 
+ */
+
+/*
+
+License
+
+Copyright (c) 2022, silvio3105
+
+Access and use of this Project and its contents are granted free of charge to any Person.
+The Person is allowed to copy, modify and use The Project and its contents only for non-commercial use.
+Commercial use of this Project and its contents is prohibited.
+Modifying this License and/or sublicensing is prohibited.
+
+THE PROJECT AND ITS CONTENT ARE PROVIDED "AS IS" WITH ALL FAULTS AND WITHOUT EXPRESSED OR IMPLIED WARRANTY.
+THE AUTHOR KEEPS ALL RIGHTS TO CHANGE OR REMOVE THE CONTENTS OF THIS PROJECT WITHOUT PREVIOUS NOTICE.
+THE AUTHOR IS NOT RESPONSIBLE FOR DAMAGE OF ANY KIND OR LIABILITY CAUSED BY USING THE CONTENTS OF THIS PROJECT.
+
+This License shall be included in all functional textual files.
+
+*/
+
+// ----- INCLUDE FILES
+#include            <SHT40.h>
+#include            <string.h>
+
+
+// ----- FUNCTION DEFINITIONS
+SHT40::SHT40(uint8_t addr, extI2C i2cr, extI2C i2cw)
+{
+	address = addr;
+	I2CRead = i2cr;
+	I2CWrite = i2cw;
+	
+	memset(&data.mData, 0, sizeof(data.mData));
+}
+
+SHT40::SHT40(extI2C i2cr, extI2C i2cw)
+{
+	address = SHT40_I2C_DEF_ADDR;
+	I2CRead = i2cr;
+	I2CWrite = i2cw;
+
+	memset(&data.mData, 0, sizeof(data.mData));
+}
+
+SHT40::~SHT40(void)
+{
+	address = 0x00;
+	I2CRead = nullptr;
+	I2CWrite = nullptr;
+
+	memset(&data.mData, 0, sizeof(data.mData));   
+}
+
+
+uint8_t SHT40::measure(SHT40_meas_t type)
+{
+	I2CWrite(address, (uint8_t*)&type, 1);    
+	I2CRead(address, (uint8_t*)&data.mData, 6);
+
+	if (data.header == SHT40_NAK) return SHT40_NOK;
+	return SHT40_OK;    
+}
+
+uint8_t SHT40::temperature(uint32_t& out)
+{
+	if (!data.mData.temp) return SHT40_NOK;
+
+	out = -45 + (175 * (uint32_t)data.mData.temp / 65535);
+	data.mData.temp = 0;
+	return SHT40_OK;
+}
+
+uint8_t SHT40::rh(uint32_t& out)
+{
+	if (!data.mData.rh) return SHT40_NOK;
+
+	out =  -6 + (125 * (uint32_t)data.mData.rh / 65535);
+	if (out > 100) out = 100;
+	else if (out < 0) out = 0;
+	
+	data.mData.rh = 0;
+	return SHT40_OK;
+}
+
+uint32_t SHT40::whoAmI(void)
+{
+	uint8_t cmd = SHT40_SN;
+
+    I2CWrite(address, (uint8_t*)&cmd, 1);    
+    I2CRead(address, (uint8_t*)&data.snData, 6);
+
+	if (data.header == SHT40_NAK) return SHT40_NOK;
+
+	return (uint32_t)((data.snData.sn1 << 16) | data.snData.sn2);	
+}
+
+void SHT40::reset() const
+{
+	uint8_t cmd = SHT40_RST;
+	I2CWrite(address, (uint8_t*)&cmd, 1); 
+}
+
+// END WITH NEW LINE
---
+++ SHT40.h
@@ -0,0 +1,302 @@
+/**
+ * @file SHT40.h
+ * @author silvio3105
+ * @brief Header file for SHT40 driver.
+ * @version v0.1r1
+ * @date 2022-10-22
+ * 
+ * @copyright silvio3105 (c) 2022
+ * 
+ */
+
+/*
+
+License
+
+Copyright (c) 2022, silvio3105
+
+Access and use of this Project and its contents are granted free of charge to any Person.
+The Person is allowed to copy, modify and use The Project and its contents only for non-commercial use.
+Commercial use of this Project and its contents is prohibited.
+Modifying this License and/or sublicensing is prohibited.
+
+THE PROJECT AND ITS CONTENT ARE PROVIDED "AS IS" WITH ALL FAULTS AND WITHOUT EXPRESSED OR IMPLIED WARRANTY.
+THE AUTHOR KEEPS ALL RIGHTS TO CHANGE OR REMOVE THE CONTENTS OF THIS PROJECT WITHOUT PREVIOUS NOTICE.
+THE AUTHOR IS NOT RESPONSIBLE FOR DAMAGE OF ANY KIND OR LIABILITY CAUSED BY USING THE CONTENTS OF THIS PROJECT.
+
+This License shall be included in all functional textual files.
+
+*/
+
+#ifndef _SHT40_H_
+#define _SHT40_H_
+
+// ----- INCLUDE FILES
+#include            <stdint.h>
+
+
+// ----- DEFINES
+/**
+ * @brief NAK symbol.
+ * @warning User should not alter this value! 
+ * 
+ */
+#define SHT40_NAK				(0x15)
+
+/**
+ * @brief Return value representing negativ OK status.
+ * 
+ */
+#define SHT40_NOK				(0x00)
+
+/**
+ * @brief Return value representing OK status.
+ * 
+ */
+#define SHT40_OK				(0x01)
+
+/**
+ * @brief Macro definition for default SHT40 I2C address.
+ * 
+ */
+#define SHT40_I2C_DEF_ADDR      (0x44) // For SHT40-AD
+//#define SHT40_I2C_DEF_ADDR      (0x45) // For SHT40-B
+
+/**
+ * @brief High precision temperature & RH measurement without heater.
+ * 
+ */
+#define SHT40_M_TRH_HP			(0xFD)
+
+/**
+ * @brief Medium precision temperature & RH measurement without heater.
+ * 
+ */
+#define SHT40_M_TRH_MP			(0xF6)  
+
+/**
+ * @brief Low precision temperature & RH measurement without heater.
+ * 
+ */
+#define SHT40_M_TRH_LP			(0xE0)  
+
+/**
+ * @brief Serial number command.
+ * 
+ */
+#define SHT40_SN				(0x89)  
+
+/**
+ * @brief Soft reset command.
+ * 
+ */
+#define SHT40_RST				(0x94)  
+
+/**
+ * @brief High precision temperature & RH measurement with 0.2W heater 1s.
+ * 
+ */
+#define SHT40_TRH_H_H02W1S		(0x39)  
+
+/**
+ * @brief High precision temperature & RH measurement with 0.2W heater 0.1s.
+ * 
+ */
+#define SHT40_TRH_H_H02W01S		(0x32)  
+
+/**
+ * @brief High precision temperature & RH measurement with 0.1W heater 1s.
+ * 
+ */
+#define SHT40_TRH_H_H011W1S		(0x2F)  
+
+/**
+ * @brief High precision temperature & RH measurement with 0.1W heater 0.1s.
+ * 
+ */
+#define SHT40_TRH_H_H011W01S	(0x24)  
+
+/**
+ * @brief High precision temperature & RH measurement with 0.02W heater 1s.
+ * 
+ */
+#define SHT40_TRH_H_H002W1S		(0x1E)  
+
+/**
+ * @brief High precision temperature & RH measurement with 0.02W heater 0.1s.
+ * 
+ */
+#define SHT40_TRH_H_H002W01S	(0x15)  
+
+
+// ----- ENUMATORS
+/**
+ * @brief Enum for measurement types
+ * 
+ */
+typedef enum: uint8_t {
+	TRH_H = SHT40_M_TRH_HP,
+	TRH_M = SHT40_M_TRH_MP,
+	TRH_L = SHT40_M_TRH_LP,
+	TRH_H_H02W1S = SHT40_TRH_H_H02W1S,
+	TRH_H_H02W01S = SHT40_TRH_H_H02W01S,
+	TRH_H_H011W1S = SHT40_TRH_H_H011W1S,
+	TRH_H_H011W01S = SHT40_TRH_H_H011W01S,
+	TRH_H_H002W1S = SHT40_TRH_H_H002W1S,
+	TRH_H_H002W01S = SHT40_TRH_H_H002W01S
+} SHT40_meas_t;
+
+
+// ----- TYPEDEFS
+/**
+ * @brief Typedef for external I2C function.
+ * 
+ * @param addr An address of I2C slave device.
+ * @param *data Pointer to data array
+ * @param len Lenth of array pointed with *data
+ * 
+ */
+typedef void (*extI2C)(uint8_t addr, uint8_t* data, uint8_t len); 
+
+
+// ----- CLASSES
+/**
+ * @brief SHT40 Class
+ * 
+ */
+class SHT40 {
+	// Public stuff
+	public:
+
+	/**
+	 * @brief Object constructor.
+	 * 
+	 * @param addr Address of I2C slave device.
+	 * @param i2cr Pointer to external I2C read function.
+	 * @param i2cw Pointer to external I2C write function. 
+	 * @return No return value.
+	 * 
+	 * @see SHT40(extI2C, extI2C)
+	 * 
+	 */
+	SHT40(uint8_t addr, extI2C i2cr, extI2C i2cw);
+
+	/**
+	 * @brief Overloaded object constructor.
+	 * 
+	 * @param i2cr Pointer to external I2C read function.
+	 * @param i2cw Pointer to external I2C write function. 
+	 * @return No return value.
+	 * 
+	 * @see SHT40(uint8_t, extI2C, extI2C)
+	 * 
+	 */
+	SHT40(extI2C i2cr, extI2C i2cw);
+
+	/**
+	 * @brief Object deconstructor.
+	 * 
+	 * @return No return value.
+	 * 
+	 */
+	~SHT40(void);
+
+	/**
+	 * @brief Measures temerature and RH.
+	 * 
+	 * @param type Type of measurement. 
+	 * @return uint8_t Returns @SHT40_NOK if NAK/NACK is received (SHT40 is busy)
+	 */
+	uint8_t measure(SHT40_meas_t type);
+
+	/**
+	 * @brief Calculates temperature from last measure call.
+	 * 
+	 * @param &out Output variable for temperature in °C.
+	 * @return Returns @SHT40_NOK if measurement data does not exist.
+	 */
+	uint8_t temperature(uint32_t& out);
+
+	/**
+	 * @brief Calculates RH from last measure call.
+	 * 
+	 * @param &out Output variable for relative humidity in %.
+	 * @return Returns @SHT40_NOK if measurement data does not exist.
+	 */
+	uint8_t rh(uint32_t& out);
+
+	/**
+	 * @brief Reads unique serial number.
+	 * 
+	 * @return uint32_t Serial Number from the chip.
+	 */
+	uint32_t whoAmI(void);	
+
+	/**
+	 * @brief Function to reset the chip.
+	 * 
+	 */
+	void reset(void) const;
+
+
+	// Private stuff
+	private:
+
+	/**
+	 * @brief Union of SHT40 data.
+	 * 
+	 */
+	union {
+		/**
+		 * @brief Header for NACK/NAK is sensor is busy.
+		 * 
+		 */
+		uint8_t header;
+
+		/**
+		 * @brief Struct for SHT40 serial number data.
+		 * 
+		 */
+		struct {
+			uint16_t sn1;
+			uint8_t crc1;
+			uint16_t sn2;
+			uint8_t crc2;		
+		} snData;
+
+		/**
+		 * @brief Struct for SHT40 measurement data.
+		 * 
+		 */
+		struct {
+			uint16_t temp;
+			uint8_t crc1;
+			uint16_t rh;
+			uint8_t crc2;
+		} mData;
+	} data;
+
+	/**
+	 * @brief I2C slave device address
+	 * 
+	 */
+	uint8_t address = SHT40_I2C_DEF_ADDR;
+
+	/**
+	 * @brief Pointer to external I2C read function.
+	 * 
+	 */
+	extI2C I2CRead = nullptr;
+
+	/**
+	 * @brief Pointer to external I2C write function.
+	 * 
+	 */
+	extI2C I2CWrite = nullptr;
+
+};
+
+
+#endif // _SHT40_H_
+
+// END WITH NEW LINE
